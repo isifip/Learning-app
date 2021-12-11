68ca273459fb6ee8ef0d17c0c64e8f317961614c
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2021-12-11 16:31:52 +0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Irakli Sokhaneishvili on 11.12.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
