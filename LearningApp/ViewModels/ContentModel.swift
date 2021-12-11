3a02393738bec920a8dd83bbb13154e6a4f41da6
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2021-12-11 16:45:11 +0400

    - Organise files into folders - Add ContentModel - Set ContentModel as EnvironmentObject

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Irakli Sokhaneishvili on 11.12.21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject, Codable {
+    
+}
